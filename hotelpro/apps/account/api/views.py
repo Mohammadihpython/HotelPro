b6f89e5cbf6e661ea4d458e9249b9fa943582d1d
Mohammadihpython <mohammadih87@yahoo.com>
2023-09-05 18:50:46 +0330

    add account app with jwt

---
+++ hotelpro/apps/account/api/views.py
@@ -0,0 +1,60 @@
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.generics import GenericAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from . import serializers
+
+user = get_user_model()
+
+
+class UserRegisterationAPIView(GenericAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = serializers.UserRegisterationSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = RefreshToken.for_user(user)
+        data = serializer.data
+        data["tokens"] = {"refresh": str(token), "access": str(token.access_token)}  # type: ignore
+        return Response(data, status=status.HTTP_201_CREATED)
+
+
+class UserLoginAPIView(GenericAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = serializers.UserLoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validate_data
+        serializer = serializers.UserSerializer(user)
+        token = RefreshToken.for_user(user)
+        data = serializer.data
+        data["token"] = {"refresh": str(token), "access": str(token.access_token)}  # type: ignore
+        return Response(data, status=status.HTTP_201_CREATED)
+
+
+class UserLogoutAPIView(GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_206_PARTIAL_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserAPIView(RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = serializers.UserSerializer
+
+    def get_object(self):
+        return self.request.user
